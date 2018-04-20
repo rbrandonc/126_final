dc5d2b102add4f026ab2c71a89be44b57f46b3b2
rbrandonc <r.brandon.c@gmail.com>
2018-04-20 11:38:56 -0400

    initial

---
+++ scripts/main.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+  // When the user scrolls the page, execute myFunction 
+  window.onscroll = function() {myFunction()};
+
+
+  // Get the navbar
+  var navbar = document.getElementById("nav");
+
+  // Get the offset position of the navbar
+  var sticky = navbar.offsetTop;
+
+  // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+  function myFunction() {
+  	console.log(window.pageYOffset, sticky)
+    if (window.pageYOffset >= sticky) {
+      navbar.classList.add("sticky")
+    } else {
+      navbar.classList.remove("sticky");
+    }
+  }
+});
