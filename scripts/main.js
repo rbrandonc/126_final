999b085de5bd1f3ba31a4b641abd348b3d5bbbac
rbrandonc <r.brandon.c@gmail.com>
2018-04-23 18:30:55 -0400

    logo

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,27 @@
 document.addEventListener("DOMContentLoaded", function(event) { 
-  // When the user scrolls the page, execute myFunction 
-  window.onscroll = function() {myFunction()};
-
+  // Detect scroll
+  window.onscroll = function() {stick()};
 
   // Get the navbar
   var navbar = document.getElementById("nav");
 
-  // Get the offset position of the navbar
-  var sticky = navbar.offsetTop;
+  // Get nav offset
+  var offset = navbar.offsetTop;
 
-  // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-  function myFunction() {
-  	console.log(window.pageYOffset, sticky)
-    if (window.pageYOffset >= sticky) {
+  //Stick the navbar when you scroll to it
+  function stick() {
+    if (window.pageYOffset >= offset) {
       navbar.classList.add("sticky")
     } else {
       navbar.classList.remove("sticky");
     }
   }
+
+  // toggle for navbar on mobile
+  var burger = document.querySelectorAll('.navbar-burger')[0];
+  burger.addEventListener('click', function () {
+      var links = document.getElementById(burger.dataset.target);
+      burger.classList.toggle('is-active');
+      links.classList.toggle('is-active');
+    });
 });
