57c6f83b9a77d34d8efd051948273909fa134086
rbrandonc <r.brandon.c@gmail.com>
2018-04-24 12:40:23 -0400

    nav fix

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,33 @@
 document.addEventListener("DOMContentLoaded", function(event) { 
+  document.getElementById('navGoesHere').innerHTML = `
+  <nav class="navbar is-primary has-text-white" id='nav'>
+    <div class="container">
+      <div class="navbar-brand">
+        <a class="navbar-item has-text-weight-bold is-uppercase" href="../">
+          Carolina Barbell
+        </a>
+        <span class="navbar-burger burger" data-target="navbarMenu">
+          <span></span>
+          <span></span>
+          <span></span>
+        </span>
+      </div>
+
+      <div id="navbarMenu" class="navbar-menu">
+        <div class="navbar-end">
+          <div class="tabs is-right">
+            <ul>
+              <li class="is-active"><a>Home</a></li>
+              <li><a href="aboutUs.html">About Us</a></li>
+              <li><a href="">Link</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+  `
+
   // Detect scroll
   window.onscroll = function() {stick()};
 
